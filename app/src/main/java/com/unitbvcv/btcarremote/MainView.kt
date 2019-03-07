b5a2417ec3c111bb32ccdf2dd74f9d4c019d23a8
Vlad Vrabie <vladvrabie97@gmail.com>
2019-03-07 19:16:03 +0200

    Removed landscape constraint. Implemented background circle drawing with dynamic radius.

--- app/src/main/java/com/unitbvcv/btcarremote/MainView.kt
+++ app/src/main/java/com/unitbvcv/btcarremote/MainView.kt
@@ -4,15 +4,12 @@ import android.content.Context
 import android.graphics.Canvas
 import android.graphics.Color
 import android.graphics.Paint
-import android.os.Debug
 import android.util.AttributeSet
 import android.util.Log
 import android.view.View
+import kotlin.math.min
 
 class MainView(context: Context, attrs: AttributeSet) : View(context, attrs) {
-
-    /* Attribute */
-//    private var _exampleString: String? = null
 
     /* Property */
 //    var exampleString: String?
@@ -26,18 +23,36 @@ class MainView(context: Context, attrs: AttributeSet) : View(context, attrs) {
         setWillNotDraw(false)
     }
 
+    private val _painter: Paint = Paint()
 
-    private val _painter: Paint = Paint().apply {
-        color = Color.BLACK
-        textSize = 100f
-        style = Paint.Style.STROKE
+    private var centerX: Float = 0f
+    private var centerY: Float = 0f
+    private val maxRadius: Float = 400f
+    private var backCircleRadius: Float = 200f
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+
+        centerX = w.toFloat() / 2
+        centerY = h.toFloat() / 2
+
+        val minSide = min(w.toFloat(), h.toFloat())
+        backCircleRadius = if (minSide / 2 < maxRadius) minSide / 2 else maxRadius
+
+        for (i in 1..100){
+            Log.i("Test: minSide = ", minSide.toString())
+            Log.i("Test: backCircleRad = ", backCircleRadius.toString())
+        }
+
+        invalidate()
     }
 
     override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
 
-        canvas.drawText("Test", 200f, 200f, _painter)
-        canvas.drawCircle(0f, 0f, 100f, _painter)
+        canvas.drawCircle(centerX, centerY, backCircleRadius, _painter.apply {
+            color = Color.GRAY
+        })
     }
 
 }
