5ab47c82f0802be94d48b8c3c40c0ede1f5b22ca
Vlad Vrabie <vladvrabie97@gmail.com>
2019-03-10 10:46:48 +0200

    Added constrained movement of the joystick inside the background circle.

--- app/src/main/java/com/unitbvcv/btcarremote/MainView.kt
+++ app/src/main/java/com/unitbvcv/btcarremote/MainView.kt
@@ -6,6 +6,7 @@ import android.graphics.Color
 import android.util.AttributeSet
 import android.view.MotionEvent
 import android.view.View
+import kotlin.math.max
 import kotlin.math.min
 
 class MainView(context: Context, attrs: AttributeSet) : View(context, attrs), View.OnTouchListener {
@@ -49,10 +50,29 @@ class MainView(context: Context, attrs: AttributeSet) : View(context, attrs), Vi
     }
 
     override fun onTouch(v: View?, event: MotionEvent?): Boolean {
+        val maxPermittedDistance = backCircle.radius - frontCircle.radius
         when (event?.action) {
-            MotionEvent.ACTION_DOWN, MotionEvent.ACTION_MOVE -> {
-                frontCircle.centerX = event.x
-                frontCircle.centerY = event.y
+            MotionEvent.ACTION_DOWN -> {
+                val x = event.x - backCircle.centerX
+                val y = event.y - backCircle.centerY
+                if (x*x + y*y <= maxPermittedDistance * maxPermittedDistance) {
+                    frontCircle.centerX = event.x
+                    frontCircle.centerY = event.y
+                }
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val x = event.x - backCircle.centerX
+                val y = event.y - backCircle.centerY
+                if (x*x + y*y <= maxPermittedDistance * maxPermittedDistance) {
+                    frontCircle.centerX = event.x
+                    frontCircle.centerY = event.y
+                }
+                else if (!(frontCircle.centerX == backCircle.centerX && frontCircle.centerY == backCircle.centerY &&
+                            x*x + y*y > backCircle.radius * backCircle.radius)) {
+                    val slopeAngle = (Math.atan2(y.toDouble(), x.toDouble()) + 2 * Math.PI)
+                    frontCircle.centerX = maxPermittedDistance * Math.cos(slopeAngle).toFloat() + backCircle.centerX
+                    frontCircle.centerY = maxPermittedDistance * Math.sin(slopeAngle).toFloat() + backCircle.centerY
+                }
             }
             MotionEvent.ACTION_UP -> {
                 frontCircle.centerX = backCircle.centerX
