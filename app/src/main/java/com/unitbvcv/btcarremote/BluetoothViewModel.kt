1627c7918ef8a98e329455992c99be79c9a8e4b2
Vlad Vrabie <vladvrabie97@gmail.com>
2019-05-05 14:11:44 +0300

    Added functions to translate joystick data to motor values.

--- app/src/main/java/com/unitbvcv/btcarremote/BluetoothViewModel.kt
+++ app/src/main/java/com/unitbvcv/btcarremote/BluetoothViewModel.kt
@@ -4,6 +4,7 @@ import android.arch.lifecycle.MutableLiveData
 import android.arch.lifecycle.Observer
 import android.arch.lifecycle.ViewModel
 import android.bluetooth.BluetoothDevice
+import kotlin.experimental.or
 
 class BluetoothViewModel : ViewModel() {
 
@@ -14,6 +15,9 @@ class BluetoothViewModel : ViewModel() {
     val joystickObserver: Observer<Pair<Double, Double>> = Observer { joystickPair: Pair<Double, Double>? ->
         // TODO: send Bluetooth data
         // or move it to a bluetooth manager
+        if (joystickPair != null) {
+            val motorValues = toMotorValues(joystickPair)
+        }
     }
 
     val ledToggle: MutableLiveData<Boolean> = MutableLiveData<Boolean>().also {
@@ -36,4 +40,69 @@ class BluetoothViewModel : ViewModel() {
         it.value = 10
     }
 
+
+    private fun toMotorValues(joystickPair: Pair<Double, Double>): Pair<Byte, Byte> {
+
+        val percentage = joystickPair.first
+        val angle = joystickPair.second
+
+        val alpha = 45.0
+        val signFlag: Byte = 0x10000000.toByte()
+
+        if (angle in 0.0..90.0) {
+            // First quadrant
+            return toMotorValuesFirstQuadrant(percentage, angle, alpha)
+        }
+        else if (angle in 90.0..180.0) {
+            // Second quadrant
+            val firstQValues = toMotorValuesFirstQuadrant(percentage, 180.0 - angle, alpha)
+            return Pair(firstQValues.second, firstQValues.first)
+        }
+        else if (angle in 180.0..270.0) {
+            // Third quadrant
+            val firstQValues = toMotorValuesFirstQuadrant(percentage, angle - 180.0, alpha)
+            return Pair(firstQValues.second.or(signFlag), firstQValues.first.or(signFlag))
+        }
+        else if (angle in 270.0..360.0) {
+            // Fourth quadrant
+            val firstQValues = toMotorValuesFirstQuadrant(percentage, 360.0 - angle, alpha)
+            return Pair(firstQValues.first.or(signFlag), firstQValues.second.or(signFlag))
+        }
+
+        return Pair(0, 0)
+    }
+
+
+    private fun toMotorValuesFirstQuadrant(percentage: Double, angle: Double, alpha: Double): Pair<Byte, Byte> {
+
+        val middleDelta = 2.0
+        val signFlag: Byte = 0x10000000.toByte()
+
+        if (angle >= 0) {
+            if (angle <= 5.0) {
+                // cazul 5
+                return Pair(percentage.toByte(), percentage.toByte().or(signFlag))
+            }
+            else if (angle <= alpha - middleDelta) {
+                // cazul 4
+                val right = (angle - 5.0) / (alpha - middleDelta - 5.0) * percentage
+                return Pair(percentage.toByte(), right.toByte().or(signFlag))
+            }
+            else if (angle <= alpha + middleDelta) {
+                // cazul 3
+                return Pair(percentage.toByte(), 0)
+            }
+            else if (angle <= 85.0) {
+                // cazul 2
+                val right = (angle - alpha - middleDelta) / (85.0 - alpha - middleDelta) * percentage
+                return Pair(percentage.toByte(), right.toByte())
+            }
+            else if (angle <= 90) {
+                // cazul 1
+                return Pair(percentage.toByte(), percentage.toByte())
+            }
+        }
+
+        return Pair(0, 0)
+    }
 }
